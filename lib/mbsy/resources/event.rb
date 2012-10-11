73677f908135e4b79b8a7fe07d70bbe754e86794
joel beasley <jbeasley6651@gmail.com>
2012-10-11 17:08:42 -0300

    Update lib/mbsy/resources/event.rb

--- lib/mbsy/resources/event.rb
+++ lib/mbsy/resources/event.rb
@@ -5,6 +5,8 @@ module Mbsy
       raise ArgumentError, "You must include :campaign_uid" unless params[:campaign_uid]
       raise ArgumentError, "You must include :email" unless params[:email]
       raise ArgumentError, "You must include :short_code" unless params[:short_code]
+      raise ArgumentError, "You must include :revenue" unless params[:revenue]
+      raise ArgumentError, "You must include :auto_create" unless params[:auto_create]
       call('record', params)
     end
     
